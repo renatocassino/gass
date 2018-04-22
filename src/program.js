3f5b39d1eb482058960c4ad1686703e7e8917495
Tacnoman <renatocassino@gmail.com>
2018-04-22 14:00:08 -0300

    WIP - Refactor

--- src/program.js
+++ src/program.js
@@ -2,7 +2,7 @@ let program = require('commander')
 
 program
   .version('0.1.0')
-  .option('-f, --files [name]', 'Glob of scss files')
+  .option('-f, --files [glob]', 'Glob of scss files')
 
 program.parse(process.argv)
 
--- src/search.js
+++ src/search.js
@@ -1,35 +1,60 @@
 const getQueryFromArray = require('./query/getQueryFromArray')
 const printResult = require('./printer')
 
+const interpreter = {}
+interpreter['word'] = function({
+  value,
+  line,
+  file,
+  query,
+  isAttribute,
+  fifo
+}) {
+  if(isAttribute) {
+    return {
+      isAttribute: !isAttribute
+    }
+  }
+
+  fifo.push(value)
+
+  if(getQueryFromArray(fifo) === query) {
+    printResult({
+      file: file,
+      line: line
+    })
+  }
+  return {
+    fifo
+  }
+}
+
+interpreter['ident'] = function({ isAttribute }) {
+  return {
+    isAttribute: !isAttribute
+  }
+}
+
+interpreter['}'] = function({ fifo }) {
+  fifo.pop()
+  return {
+    fifo
+  }
+}
+
 const search = async function(query, file, tokens) {
-  let filo = []
-  let isAttribute = false
-
+  let state = {
+    isAttribute: false,
+    fifo: []
+  }
   tokens.forEach(function(token) {
-    if(token[0] === 'word') {
-      if(isAttribute) {
-        isAttribute = !isAttribute
-        return
-      }
+    const [ type, value, line ] = token
+    const { isAttribute, fifo } = state
 
-      filo.push(token[1])
-
-      if(getQueryFromArray(filo) === query) {
-        printResult({
-          file: file,
-          line: token[2]
-        })
-      }
-      return
-    }
-
-    if(token[0] === 'ident') {
-      isAttribute = !isAttribute
-      return
-    }
-
-    if(token[0] === '}') {
-      filo.pop()
+    if(typeof interpreter[type] !== 'undefined') {
+      state = Object.assign({}, state, interpreter[type]({
+        value, line, file, query, fifo, isAttribute
+      }))
       return
     }
   })
