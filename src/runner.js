93adfd411842d31f84541bc1a478114d685ad97f
Renato Cassino <renato.santos@corp.globo.com>
2018-04-24 13:14:56 -0300

    Fix bug in path

--- src/runner.js
+++ src/runner.js
@@ -4,11 +4,19 @@ const colors = require('colors')
 const readFile = require('./readFile')
 
 const run = function() {
-  const globFiles = program.files || '**/*.scss'
+  let globFiles = program.files || '**/*.scss'
+  if (globFiles[0] !== '/') {
+    globFiles = process.cwd() + '/' + globFiles
+  }
 
   console.log(colors.yellow('Searching in: ' + globFiles + '\n'))
 
   glob(globFiles, function (er, files) {
+    if(er) {
+      console.log(er.message)
+      process.exit()
+    }
+
     files.forEach(readFile)
   })
 }
