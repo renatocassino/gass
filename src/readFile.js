8fa90048519b57113f8fc902a4bb8258d975ad6a
Cassino <renatocassino@gmail.com>
2020-11-12 11:46:54 -0300

    feat(feedback): Add message errors

--- src/readFile.js
+++ src/readFile.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const colors = require('colors')
 const ScssTokenizer = require('tacnoman-scss-tokenizer')
 const parser = require('./parser')
 const printResults = require('./printer').printResults
@@ -7,6 +8,11 @@ const classToSearch = process.argv[2]
 
 const readFile = (file) => new Promise((resolve) => {
   fs.readFile(file, 'utf-8', async function(err, data) {
+    if (err) {
+      console.log(colors.red(err.message))
+      resolve([])
+    }
+
     const tokens = ScssTokenizer.tokenize(data)
     const results = parser(tokens, classToSearch, file)
 
